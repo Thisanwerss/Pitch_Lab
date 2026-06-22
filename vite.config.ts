f252bce71e86c1f1d996ee1e7eaf85b9493b8f37
Dian Yu <imdian.yu@tum.de>
2026-06-22 11:44:48 +0200

    Fix GitHub Pages root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: process.env.GITHUB_PAGES_BASE ?? "/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].js",
+      },
+    },
+  },
   plugins: [react()],
 });
